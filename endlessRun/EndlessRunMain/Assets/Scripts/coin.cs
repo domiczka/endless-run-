dbb2edaabff9be65dba5e8644555de8440b0e6fd
domiczka <dominika.soroka2@gmail.com>
2020-11-09 20:19:40 +0100

    coin etap pierwszy dominika

---
+++ endlessRun/EndlessRunMain/Assets/Scripts/coin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+
+    public float turnSpeed = 90f;                    // szybkość obracania monety  
+    
+    void OnTriggerEnter (Collider other)
+
+    {
+        // sprawdzamy czy colliderem jest gracz
+        if (other.gameObject.name != "Player")  
+        {
+            return;                                  //tzn jeśli colliderem nie jest gracz to zatrzymujemy funkcję
+        }
+
+
+        //dodajemy punkt (za zebranie monety)
+
+        //niszczymy monetę
+        Destroy(gameObject);
+
+    }
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);   //obracanie monety
+    }
+}
