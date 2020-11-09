56848e50abf861beacca239a9f76ae6de5edcaac
domiczka <dominika.soroka2@gmail.com>
2020-11-09 21:35:11 +0100

    player and camera movement

---
+++ endlessRun/EndlessRunMain/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;                //dzięki temu gracz porusza się horyzontalnie (prawo, lewo) 2 raza szybciej niz porusza się do przodu
+
+
+   private void FixedUpdate ()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;   //kliknięcie klawisza d powoduje przesuwanie gracza w prawo, a klawisza a - w lewo
+        
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
