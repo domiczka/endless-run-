f1bb59e4b5a55a89d1128dde5bb3e2aae550a6d3
tXxy <xxyxxyyyx@gmail.com>
2020-11-24 00:12:48 +0100

    coinDespawnFix

---
+++ endlessRun/EndlessRunMain/Assets/deleteObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteObjects : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("temp"))
+        {
+            Debug.Log("Test123");
+            Destroy(other.gameObject, 2);
+        }
+    }
+}
