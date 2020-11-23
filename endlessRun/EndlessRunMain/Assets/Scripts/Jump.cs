995dee1e2ac2df3cc2aae6014b4e5bb9b0dea5ed
KrystianOśka <krystianoska@gmail.com>
2020-11-23 22:33:32 +0100

    Skakanie

---
+++ endlessRun/EndlessRunMain/Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    private float jumpSpeed = 6;
+    private Rigidbody rigidBody;
+    private bool onGround = true;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && onGround)
+        {
+            rigidBody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+
+}
